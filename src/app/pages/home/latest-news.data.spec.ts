8fead7d9b965b02d1ae577331669a85345c4456d
JonathanWassef <jonathanwassef60@gmail.com>
2026-07-19 01:09:28 -0500

    Add event for 2026 Liturgy and Life Convention

--- src/app/pages/home/latest-news.data.spec.ts
+++ src/app/pages/home/latest-news.data.spec.ts
@@ -15,6 +15,7 @@ describe('latestNewsCards', () => {
     dateLabel: 'August 22, 2026',
     cta: 'Join us in prayer as we celebrate the Assumption of the Body of St. Mary on Saturday, August 22.',
   },
+  {
     title: '2026 Liturgy and Life Convention',
     dateLabel: 'November 6-8, 2026',
     cta: "Tickets go on sale: August 2 - September 27. Visit: events.ticketleap.com/tickets/liturgy-and-life-convention/2026-liturgy-and-life-
