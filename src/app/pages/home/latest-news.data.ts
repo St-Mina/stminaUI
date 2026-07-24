8e611dd9d42220f32b822d6e794c042fbb9a1bf6
JonathanWassef <jonathanwassef60@gmail.com>
2026-07-23 20:12:16 -0500

    Update latest news data and remove dateLabel
    Removed dateLabel from LatestNewsCard interface and updated latestNewsCards with new titles and CTAs.

--- src/app/pages/home/latest-news.data.ts
+++ src/app/pages/home/latest-news.data.ts
@@ -1,23 +1,19 @@
 export interface LatestNewsCard {
   readonly title: string;
-  readonly dateLabel: string;
   readonly cta: string;
 }
 
 export const latestNewsCards: readonly LatestNewsCard[] = [
   {
-    title: 'The Feast of Transfiguration',
-    dateLabel: 'August 19, 2026',
-    cta: 'Join us in prayer as we celebrate the Feast of Transfiguration on Wednesday, August 19',
+    title: 'Two Liturgies every Sunday',
+    cta: 'We have two liturgies every Sunday. The Arabic Liturgy at 5:30am, and the English Liturgy at 8:30am.',
   },
   {
-    title: 'Assumption of the Body of St. Mary',
-    dateLabel: 'August 22, 2026',
-    cta: 'Join us in prayer as we celebrate the Assumption of the Body of St. Mary on Saturday, August 22.',
+    title: 'Vespers and Midnight Praises every Saturday',
+    cta: 'Join us for Vespers and Midnight Praises every Saturday starting at 5:30pm.',
   },
   {
-    title: '2026 Liturgy and Life Convention',
-    dateLabel: 'November 6-8, 2026',
-    cta: "Tickets go on sale: August 2 - September 27. Visit: https://events.ticketleap.com/tickets/liturgy-and-life-convention/2026-liturgy-and-life-convention",
+    title: 'Family Meeting and Elementary/Middle/High School Bible Study',
+    cta: 'Come to our Bible Study fellowships on Friday. All services begin at 6:30pm.',
   },
 ] as const;
