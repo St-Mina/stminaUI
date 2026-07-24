5f4f0d42f9aeea93ed028b9b2c35e174f4998756
JonathanWassef <jonathanwassef60@gmail.com>
2026-07-23 20:29:17 -0500

    Update titles for latest news cards to be consistent

--- src/app/pages/home/latest-news.data.ts
+++ src/app/pages/home/latest-news.data.ts
@@ -5,15 +5,15 @@ export interface LatestNewsCard {
 
 export const latestNewsCards: readonly LatestNewsCard[] = [
   {
-    title: 'Two Liturgies every Sunday',
+    title: 'Two Liturgies Every Sunday',
     cta: 'We have two liturgies every Sunday. The Arabic Liturgy at 5:30am, and the English Liturgy at 8:30am.',
   },
   {
-    title: 'Vespers and Midnight Praises every Saturday',
+    title: 'Vespers and Midnight Praises Every Saturday',
     cta: 'Join us for Vespers and Midnight Praises every Saturday starting at 5:30pm.',
   },
   {
-    title: 'Family Meeting and Elementary/Middle/High School Bible Study',
+    title: 'Family Meeting and Elementary/Middle/High School Bible Study Every Friday',
     cta: 'Come to our Bible Study fellowships on Friday. All services begin at 6:30pm.',
   },
 ] as const;
