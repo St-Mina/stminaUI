807d354098cf37fd8410e169d1bd1cbf171dbb2d
JonathanWassef <jonathanwassef60@gmail.com>
2026-08-20 08:54:16 -0500

    Add video playback functionality to About component

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About {}
+export class About {
+  historyVideoPlaying = false;
+
+  historyVideoId = 'X7HBTiH7CHE';
+  historyVideoThumbnail =
+    'https://img.youtube.com/vi/X7HBTiH7CHE/maxresdefault.jpg';
+
+  playHistoryVideo(): void {
+    this.historyVideoPlaying = true;
+  }
+}
