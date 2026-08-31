298ceaf69e1ae033fea4597087ba28b5c1266cda
JonathanWassef <jonathanwassef60@gmail.com>
2026-08-31 12:02:06 -0500

    Delete src/app/pages/services directory

--- src/app/pages/services/services.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-services',
-  standalone: true,
-  templateUrl: './services.html',
-  styleUrl: './services.scss',
-})
-export class Services {}
