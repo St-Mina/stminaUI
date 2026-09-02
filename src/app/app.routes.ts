6123cced450252381c5181f59703a8b8c5d03d15
JonathanWassef <jonathanwassef60@gmail.com>
2026-09-02 12:44:11 -0500

    Restore and add routes in app.routes.ts
    Restored 'announcements' route and added 'gallery' routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,10 +14,6 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/about/about').then((m) => m.About),
   },
   {
-    path: 'announcements',
-    loadComponent: () => import('./pages/announcements/announcements').then((m) => m.Announcements),
-  },
-  {
     path: 'give',
     loadComponent: () => import('./pages/give/give').then((m) => m.Give),
   },
@@ -26,20 +22,20 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/livestream/livestream').then((m) => m.Livestream),
   },
   {
-    path: 'gallery',
-    loadComponent: () => import('./pages/gallery/gallery').then((m) => m.Gallery),
-  },
-  {
-  path: 'gallery/BishopPhilopateer2026',
-  loadComponent: () => import('./pages/gallery/BishopPhilopateer2026/component').then((m) => m.BishopPhilopateer2026),
-  },
-  {
     path: 'contact',
     loadComponent: () => import('./pages/contact/contact').then((m) => m.Contact),
   },
   {
     path: 'prayer-request',
     loadComponent: () => import('./pages/prayer-request/component').then((m) => m.PrayerRequestComponent),
+  },
+  {
+    path: 'gallery',
+    loadComponent: () => import('./pages/gallery/gallery').then((m) => m.Gallery),
+  },
+  {
+  path: 'gallery/BishopPhilopateer2026',
+  loadComponent: () => import('./pages/gallery/BishopPhilopateer2026/component').then((m) => m.BishopPhilopateer2026),
   },
   {
     path: '**',
